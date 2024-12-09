5b58558b3dc317ba16ca7512c68f7fef551c2789
Vasily <31712700+VasilyVP@users.noreply.github.com>
2024-12-09 17:46:39 -0300

    tRPC local config fix

--- front-end/src/config.ts
+++ front-end/src/config.ts
@@ -1,15 +1,13 @@
 export const isServer = typeof window === "undefined";
 export const isClient = typeof window !== "undefined";
 
-if (!process.env.NEXT_PUBLIC_TRPC_SERVER) {
-    throw new Error("NEXT_PUBLIC_TRPC_SERVER is not defined");
-}
+
 if (isServer && !process.env.DRIZZLE_DATABASE_URL) {
     console.log('process.env.DRIZZLE_DATABASE_URL (inside): ', process.env.DRIZZLE_DATABASE_URL);
     throw new Error("DRIZZLE_DATABASE_URL is not defined");
 }
 
 export const config = {
-    trpcServerLocal: process.env.NEXT_PUBLIC_TRPC_SERVER,
+    trpcServerLocal: process.env.NEXT_PUBLIC_TRPC_SERVER, // only for local dev
     drizzleDatabaseUrl: process.env.DRIZZLE_DATABASE_URL!,
 }
