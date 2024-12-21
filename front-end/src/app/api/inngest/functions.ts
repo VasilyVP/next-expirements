34a539ee340cefdbe30046459fea26d2fa51f49a
Vasily <31712700+VasilyVP@users.noreply.github.com>
2024-12-21 17:15:55 -0300

    last update

--- front-end/src/app/api/inngest/functions.ts
+++ front-end/src/app/api/inngest/functions.ts
@@ -1,23 +1,28 @@
-import { loggerLink } from "@trpc/client";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
+    {
+        id: "hello-world",
+        retries: 2,
+    },
     { event: "test/hello.world" },
-    async ({ event, step }) => {
+    async ({ event, step, attempt }) => {
         console.log('in the function');
 
-        await step.sleep("wait-a-minute", "10s");
+        console.log('attempt: ', attempt);
 
+        await step.sleep("wait-a-minute", "10s");
         console.log('after sleep');
 
-        await step.waitForEvent("wait-for-event", { event: "test/hello.world-2", timeout: "2m" });
-
-        console.log('after waiting for event');
+        //await step.waitForEvent("wait-for-event", { event: "test/hello.world-2", timeout: "2m" });
+        //console.log('after waiting for event');
 
         await step.run('test/hello.world-3', () => {
             console.log('test/hello.world-3 executed');
+            if (attempt < 2) throw Error('Some error');
         });
+
+        console.log('after run');
 
         return { message: `Hello ${event.data.email}!` };
     }
